e4892e6e42eb9e94fef0d2d014af2a6f160e125e
Manuel Marcos <38569631+ManuMarcos@users.noreply.github.com>
2020-04-09 08:59:35 -0300

    Delete TP2_exercise2.py

--- TP2_exercise2.py
+++
@@ -1,8 +0,0 @@
-print("Welcome!!")
-a = int(input("Please enter a first number:"))
-b = int(input("Please enter second number:"))
-sum = a + b
-substraction = a - b
-print("The result of sum is:", sum)
-print("The result of substraction is:", substraction)
-
